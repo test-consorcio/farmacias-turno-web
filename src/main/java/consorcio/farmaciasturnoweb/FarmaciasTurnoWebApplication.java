3377ff85ce483a910143bd05627e7f21a7b401af
rodrigo <rodrigo@rox.roxdom>
2020-05-28 15:53:47 -0400

    Web para ver las farmacias de turno en la región metropolitana.

---
+++ src/main/java/consorcio/farmaciasturnoweb/FarmaciasTurnoWebApplication.java
@@ -0,0 +1,13 @@
+package consorcio.farmaciasturnoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FarmaciasTurnoWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FarmaciasTurnoWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/consorcio/farmaciasturnoweb/FarmaciasTurnoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package consorcio.farmaciasturnoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmaciasTurnoWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
